debadcf8596a396ab41f2204a4d999e56a97aad8
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-09-05 11:41:24 +0100

    Mudanda o valor da variavel message, usando useState: my way

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
+
 function App() {
-  const message = "Ola Mundo do React";
+  const [message, setMessage] = useState("Ola Mundo do React");
+  // setMessage("Vou ser melhor nesse mambo");
+  const newMessage = () => {
+    setMessage("Serei bom nesse mambo");
+  };
   return (
     <div>
       <h1>Foco nos estudos {message}</h1>
+      <button onClick={newMessage}>Mudar sms</button>
     </div>
   );
 }
