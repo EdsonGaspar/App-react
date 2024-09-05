7144e1f3a6b7c22760d10af98f2b845c65ce50ae
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-09-05 11:52:23 +0100

    Mostrando a diferenca entre uma vaiavel normal e um useState, um useState actualiza o componente renderizado na tela a cada mudanca diferente de usar uma variavel normal.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 
 function App() {
   const [message, setMessage] = useState("Ola Mundo do React");
+  let text = "Angola Vante";
   // setMessage("Vou ser melhor nesse mambo");
   const newMessage = () => {
+    text = "Pais vai mudar para melhor, a dona Claudia acredita nisso.";
     setMessage("Serei bom nesse mambo");
+    console.log(text);
   };
   return (
     <div>
       <h1>Foco nos estudos {message}</h1>
+      <h1>Foco no desenvolvimento {text}</h1>
       <button onClick={newMessage}>Mudar sms</button>
     </div>
   );
