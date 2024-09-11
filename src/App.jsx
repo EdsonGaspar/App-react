ea75479b14b3308cc23b46c48db1fe2309b74cbe
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-09-11 21:34:17 +0100

    Finished the configuration TailwindCss in this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import AddTasks from "./components/AddTasks";
+import ComponentText from "./components/ConponentText";
 import Tasks from "./components/Tasks";
+import "./index.css";
 
 function App() {
   return (
     <div>
-      <h1>Lista de Tarefas</h1>
+      <h1 className="text-red-500 bg-black">Lista de Tarefas</h1>
       <AddTasks />
       <Tasks />
+      <ComponentText />
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,5 @@
 function Tasks() {
-  return <h1>Mostrar Lista de tarefas</h1>;
+  return <h1 className="bg-black text-white">Mostrar Lista de tarefas</h1>;
 }
 
 export default Tasks;
