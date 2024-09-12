fc16523c51ed138a92b305313b2666d8bd1fd863
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-09-12 10:56:41 +0100

    In this branch, I develop an aplication that do receive text and change in the same moment, there are one button to restart

--- src/components/ChangeText.jsx
+++ src/components/ChangeText.jsx
@@ -1,3 +1,26 @@
+import { useState } from "react";
+
 export function ChangeText() {
-  return <div>Mudar o valor do texto</div>;
+  const [start, setStart] = useState("Hello");
+  function stateInicial(e) {
+    setStart(e.target.value);
+  }
+  return (
+    <div>
+      <input
+        value={start}
+        onChange={stateInicial}
+        type="text"
+        className="bg-[#1b1b1b] text-white py-1 px-2 border rounded-md"
+        placeholder="Hello"
+      />
+      <p>Texto Escrito: {start}</p>
+      <button
+        onClick={() => setStart("Hellol")}
+        className="border rounded-sm cursor-pointer p-2 mt-2"
+      >
+        Reset
+      </button>
+    </div>
+  );
 }
