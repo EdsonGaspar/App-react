7c68269e43bb9b2753d95020da4c585cbe4e0fc0
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-09-13 22:00:26 +0100

    Feat: Adicionando a lista de tarefa apartir de uma useState, agora a tarefa e usar esta useState no componente Tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 import AddTasks from "./components/AddTasks";
-import ComponentText from "./components/ConponentText";
 import Tasks from "./components/Tasks";
 import "./index.css";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar Programação",
+      description: "Estudar programação para se tornar um full tack",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Inglês",
+      description: "Estudar inglês para se tornar fluente",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Ler um livro",
+      description: "Para para se divertir",
+      isCompleted: false,
+    },
+  ]);
   return (
-    <div>
-      <h1 className="text-red-500 bg-black">Lista de Tarefas</h1>
-      <AddTasks />
-      <Tasks />
-      <ComponentText />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] border">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Lista de Tarefas
+        </h1>
+        <AddTasks />
+        <Tasks />
+      </div>
     </div>
   );
 }
--- src/components/ConponentText.jsx
+++
@@ -1,5 +0,0 @@
-function ComponentText() {
-  return <h1 className="text-3xl font-bold underline ">Hello world!</h1>;
-}
-
-export default ComponentText;
