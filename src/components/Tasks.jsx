60962fa7c2885a8f05f39acaca1a3d6e83a100ac
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-09-15 20:07:02 +0100

    Terminei a lista de tarefas usando o Distruction no componente Tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,11 +1,11 @@
-function Tasks(props) {
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   return (
     <div>
       <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-        {props.tasks.map((task) => (
+        {tasks.map((task) => (
           <li key={task.id} className="flex text-white gap-2">
             <button
-              onClick={() => props.onTaskClick(task.id)}
+              onClick={() => onTaskClick(task.id)}
               className={`flex-1 text-left bg-slate-400 p-2 rounded-md ${
                 task.isCompleted && "line-through"
               }`}
@@ -14,7 +14,7 @@ function Tasks(props) {
             </button>
             <button className="bg-slate-400 p-2 rounded-md">Dtls</button>
             <button
-              onClick={() => props.onDeleteTaskClick(task.id)}
+              onClick={() => onDeleteTaskClick(task.id)}
               className="bg-slate-400 p-2 rounded-md"
             >
               Dlt
