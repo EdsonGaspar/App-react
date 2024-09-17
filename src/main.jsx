8345a3c42be776f90c266ae0a08604c37c99bf5a
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-09-17 14:31:05 +0100

    Estilizando a pagina TaskPage para mostrar detalhes

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TaskPage from "./page/TaskPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/task",
+    element: <TaskPage />,
   },
 ]);
 
---
+++ src/page/TaskPage.jsx
@@ -0,0 +1,7 @@
+export default function TaskPage() {
+  return (
+    <div className="h-screen w-screen bg-slate-500 p-6">
+      <h1>Pagina Detalhes</h1>
+    </div>
+  );
+}
