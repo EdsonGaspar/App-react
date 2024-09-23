8ede2917aaf54186c1a161c7dea164bf3dc72bac
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-09-23 21:16:50 +0100

    Estilização da pagina de detalhes

--- src/page/TaskPage.jsx
+++ src/page/TaskPage.jsx
@@ -6,9 +6,12 @@ export default function TaskPage() {
   const description = searchParams.get("description");
 
   return (
-    <div className="h-screen w-screen bg-slate-500 p-6">
-      <h1>{title}</h1>
-      <p>{description}</p>
+    <div className="h-screen w-screen bg-slate-500 p-6 flex flex-col items-center space-y-6">
+      <h1 className="font-bold text-white text-3xl">Detalhes da Tarefa</h1>
+      <div className="bg-slate-300 p-6 rounded-md  text-center ">
+        <h1 className="text-2xl font-semibold mb-4">{title}</h1>
+        <p className="text-slate-700">{description}</p>
+      </div>
     </div>
   );
 }
