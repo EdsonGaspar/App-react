3a478858b48d95654df076d45766f4ce0a41117a
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-09-23 21:53:32 +0100

    Alguns ajuste de tela e a codificacao do botao regressarcls

--- src/page/TaskPage.jsx
+++ src/page/TaskPage.jsx
@@ -1,16 +1,30 @@
-import { useSearchParams } from "react-router-dom";
+import { ChevronLeftIcon } from "lucide-react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export default function TaskPage() {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
 
   return (
     <div className="h-screen w-screen bg-slate-500 p-6 flex flex-col items-center space-y-6">
-      <h1 className="font-bold text-white text-3xl">Detalhes da Tarefa</h1>
-      <div className="bg-slate-300 p-6 rounded-md  text-center ">
-        <h1 className="text-2xl font-semibold mb-4">{title}</h1>
-        <p className="text-slate-700">{description}</p>
+      <div className="w-[500px] space-y-4">
+        <div className="flex justify-center relative">
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute top-0 left-0 bottom-0 text-slate-100"
+          >
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="font-bold text-white text-3xl">Detalhes da Tarefa</h1>
+        </div>
+        <div className="bg-slate-300 p-6 rounded-md  text-center ">
+          <h1 className="text-slate-700 text-2xl font-semibold mb-4">
+            {title}
+          </h1>
+          <p className="text-slate-700">{description}</p>
+        </div>
       </div>
     </div>
   );
